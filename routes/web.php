61ff137bf4057a823dcea1f1c81267c403ab32b8
omles <muhlis.ach02@gmail.com>
2024-11-16 10:38:54 +0700

    add admin

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+
+Route::get('/beranda', function () {return view('beranda');});
+
+Route::get('/kontak', function () {return view('kontak');});
+
+Route::get('/profil', function () {return view('profil');});
+
+Route::get('/admin', function () {return view('admin');});
+
+Route::get('/layanan', function () {return view('layanan');});
+
+
